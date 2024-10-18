035d234abc0c1d05d952657f1df2f27088112f36
saltfish23 <ouchmikan333@gmail.com>
2024-10-18 15:00:16 -0700

    temper tantrum robor moment

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -6,25 +6,25 @@ brain Brain;
 controller controller1 = controller(primary);
 
 // Initiate and define base motors
-motor leftFwd = motor(PORT1, ratio6_1, true);
+motor leftFwd = motor(PORT10, ratio6_1, true);
 motor leftMid = motor(PORT4, ratio6_1, false);
 motor leftBack = motor(PORT3, ratio6_1, true);
 //36 to 60
 //6 to 10
 //3 to 5
 
-motor rightFwd = motor(PORT8, ratio6_1, false);
-motor rightMid = motor(PORT19, ratio6_1, true);
+motor rightFwd = motor(PORT13, ratio6_1, false);
+motor rightMid = motor(PORT14, ratio6_1, true);
 motor rightBack = motor(PORT9, ratio6_1, false);
 
 inertial gyro4 = inertial(PORT11);
 
 //Initiate and define Intake motor
-motor intake = motor(PORT16, ratio18_1, true);
+motor intake = motor(PORT12, ratio18_1, true);
 
 //Initiate and define Lift motors
 motor lift1 = motor(PORT5, ratio18_1, false);
-motor lift2 = motor(PORT7, ratio6_1, true);
+motor lift2 = motor(PORT21, ratio6_1, true);
 
 //Initatie and define pneumatics for wings
 pneumatics clamp = pneumatics(Brain.ThreeWirePort.H);
